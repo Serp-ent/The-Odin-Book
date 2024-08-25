bdd4a6bcf6b97d03aa36b32fe6b3282e8ae9c346
kenny <kenny909@mat.umk.pl>
2024-08-25 13:04:07 +0200

    Add style for error page

--- frontend/src/routes/error-page.jsx
+++ frontend/src/routes/error-page.jsx
@@ -1,16 +1,35 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.log(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div
+      className='h-screen w-screen bg-slate-700 flex justify-center items-center'
+    >
+      <div
+        className='text-white bg-slate-800 py-6 px-8 rounded shadow-lg flex flex-col justify-between gap-2'
+      >
+        <h1>Oops!</h1>
+        <p className='text-sm'>Sorry, an unexpected error has occurred.</p>
+        <div 
+        className='flex justify-center my-2'
+        >
+          <p>
+            <i>{error.statusText || error.message}</i>
+          </p>
+        </div>
+
+        <div
+          className='flex justify-end'
+        >
+          <Link
+            to={'/'}
+            className='border-2 px-4 py-2 rounded'
+          >Take me back...</Link>
+        </div>
+      </div>
     </div>
   );
 }
