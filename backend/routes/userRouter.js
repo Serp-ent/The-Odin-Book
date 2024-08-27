7fcd9ad36e4a89df51d34e4e35fe0e92eda671a0
kenny <kenny909@mat.umk.pl>
2024-08-27 15:26:54 +0200

    Fix pattern miss matching where id was equal 'followed'

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,10 +4,12 @@ const controller = require('../controllers/userController.js');
 const passport = require('../config/passport-config.js');
 
 // router.get('/', controller.getPosts);
-router.get('/:id', controller.getUserWithId);
 
+// Define before to prevent pattern matching
 router.get('/followed',
   passport.authenticate('jwt', { session: false }),
   controller.getFollowedUsers);
+
+router.get('/:id', controller.getUserWithId);
 
 module.exports = router;
