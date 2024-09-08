c87165da91ea4070f552e154860e2ef7c3b8aeda
kenny <kenny909@mat.umk.pl>
2024-09-08 14:08:00 +0200

    Highlight currently picked route

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import odinIcon from './assets/odin-icon.svg';
 import './index.css'
 import { useAuth } from './auth/authContext';
@@ -23,7 +23,13 @@ const fetchUserInfo = async (userId) => {
   return response.json();
 };
 
-// TODO: highlight current route in popup
+const menuOptions = [
+  { path: (userId) => `/profile/${userId}`, label: 'profile' },
+  { path: '/users', label: 'findFriends' },
+  { path: '/users/followed', label: 'following' },
+  { path: '/profile/settings', label: 'settings' },
+];
+
 export default function Header() {
   const auth = useAuth();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -33,12 +39,12 @@ export default function Header() {
   const [isDarkMode, setIsDarkMode] = useState(() => {
     const storedTheme = localStorage.getItem('theme');
     return storedTheme === 'dark' || (!storedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches);
-  })
+  });
 
-  const { data: userInfo, isLoading, isError } = useQuery({
+  const { data: userInfo, isLoading } = useQuery({
     queryKey: ['userInfo', auth.userId],
     queryFn: () => fetchUserInfo(auth.userId),
-    enabled: !!auth.userId, // Only run query if userId is available
+    enabled: !!auth.userId,
   });
 
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
@@ -47,20 +53,17 @@ export default function Header() {
     const newTheme = isDarkMode ? 'light' : 'dark';
     setIsDarkMode(!isDarkMode);
     localStorage.setItem('theme', newTheme);
-  }
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         closeDropdown();
       }
-    }
+    };
 
     document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
   useEffect(() => {
@@ -69,22 +72,17 @@ export default function Header() {
     } else {
       document.documentElement.classList.remove('dark');
     }
-  }, [isDarkMode])
+  }, [isDarkMode]);
 
   if (isLoading || !ready) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
-    <header
-      className='p-2 bg-background-light text-text-primary-light dark:text-text-primary-dark dark:bg-background-dark flex items-center justify-between'
-    >
+    <header className='p-2 bg-background-light text-text-primary-light dark:text-text-primary-dark dark:bg-background-dark flex items-center justify-between'>
       <div>
-        <Link
-          className='flex justify-start items-center gap-1'
-          href='/'>
-          <img src={odinIcon}
-            className='h-10' />
+        <Link className='flex justify-start items-center gap-1' to='/'>
+          <img src={odinIcon} className='h-10' alt="The Odin Project" />
           <h1 className='font-bold hidden sm:block'>
             The Odin Book
           </h1>
@@ -96,53 +94,32 @@ export default function Header() {
           <>
             <UserHeader user={userInfo} size='small' />
             <div className='relative'>
-              <button
-                className='py-1 px-2'
-                onClick={toggleDropdown}
-              >
+              <button className='py-1 px-2' onClick={toggleDropdown}>
                 <FaBars size={"24px"} />
               </button>
 
               {isDropdownOpen && (
-                <div className='border p-2 absolute right-0 mt-2 w-48 bg-background-light text-text-primary-light dark:bg-background-dark dark:text-text-primary-dark rounded-lg shadow-lg z-50'>
+                <nav className='border p-2 absolute right-0 mt-2 w-48 bg-background-light text-text-primary-light dark:bg-background-dark dark:text-text-primary-dark rounded-lg shadow-lg z-50'>
                   <ul className='py-2 flex flex-col gap-2'>
-                    <li>
-                      <Link
-                        className='block p-1 test-white dark:hover:bg-gray-800 hover:bg-gray-200'
-                        onClick={closeDropdown}
-                        to={`/profile/${auth.userId}`}
-                      >
-                        {t('profile')}
-                      </Link>
-                    </li>
-                    <hr></hr>
-                    <Link to={'/users'}
-                      className='block p-1 test-white dark:hover:bg-gray-800 hover:bg-gray-200'>
-                      {t('findFriends')}
-                    </Link>
-                    <hr></hr>
-                    <Link to={'/users/followed'}
-                      className='block p-1 test-white dark:hover:bg-gray-800 hover:bg-gray-200'>
-                      {t('following')}
-                    </Link>
-                    <hr></hr>
-                    <li>
-                      <Link
-                        className='block p-1 test-white hover:bg-gray-200 dark:hover:bg-gray-800'
-                        to={`/profile/settings`}
-                      >
-                        {t('settings')}
-                      </Link>
-                    </li>
+                    {menuOptions.map((option, index) => (
+                      <li key={index}>
+                        <NavLink
+                          className={({ isActive }) =>
+                            `block p-1 rounded px-2 dark:hover:bg-gray-800 hover:bg-gray-200 ${isActive ? 'bg-gray-200 dark:bg-gray-800' : ''}`
+                          }
+                          onClick={closeDropdown}
+                          to={typeof option.path === 'function' ? option.path(auth.userId) : option.path}
+                        >
+                          {t(option.label)}
+                        </NavLink>
+                      </li>
+                    ))}
                     <hr></hr>
                     <li className='hover:bg-gray-200 dark:hover:bg-gray-800'>
-                      <button
-                        className='block p-1 test-white '
-                        onClick={() => {
-                          auth.logout()
-                          closeDropdown();
-                        }}
-                      >
+                      <button className='block p-1 test-white' onClick={() => {
+                        auth.logout();
+                        closeDropdown();
+                      }}>
                         {t('logout')}
                       </button>
                     </li>
@@ -150,29 +127,41 @@ export default function Header() {
 
                   <div className='flex justify-between items-center bg-background-light text-text-primary-light dark:bg-background-dark dark:text-text-primary-dark'>
                     <LanguageSelector />
-
                     <div className='flex justify-end'>
                       <button onClick={toggleDarkMode} className="ml-4 p-2 rounded">
                         {isDarkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
                       </button>
                     </div>
                   </div>
-                </div>
+                </nav>
               )}
             </div>
           </>
         ) : (
-          <div className='flex gap-2 items-center justify-end text-sm'>
-            <Link to={'/login'}>{t('login')} </Link>
-            <Link to={'/register'}>{t('register')} </Link>
+          <nav className='flex gap-2 items-center justify-end text-sm'>
+            <NavLink
+              to={'/login'}
+              className={({ isActive }) =>
+                `px-2 py-1 rounded ${isActive ? 'bg-gray-200 dark:bg-gray-800' : ''}`
+              }
+            >
+              {t('login')}
+            </NavLink>
+            <NavLink
+              to={'/register'}
+              className={({ isActive }) =>
+                `px-2 py-1 rounded ${isActive ? 'bg-gray-200 dark:bg-gray-800' : ''}`
+              }
+            >
+              {t('register')}
+            </NavLink>
             <LanguageSelector size='small' />
             <button onClick={toggleDarkMode} className="rounded">
               {isDarkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
             </button>
-          </div>
+          </nav>
         )}
-
       </div>
-    </header >
+    </header>
   );
 }
